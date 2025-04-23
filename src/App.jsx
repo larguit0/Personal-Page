fe3eaa15a8097e75411bfa3052bf81f5220a84ba
larguit0 <miguepz12@gmail.com>
2025-04-23 14:17:48 -0500

    Primer commit: estructura inicial del proyecto personal

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+
+function App(){
+  return (
+    <Router>
+      <Routes>
+        <Route path="nav" element={<Navbar></Navbar>}></Route>
+  
+
+
+      </Routes>
+    </Router>
+  )
+}
+
+export default App; 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-900 text-white fixed top-0 left-0 w-full shadow-lg z-50 rounded">
+      <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
+        <ul className="flex space-x-6">
+          <li><Link to="/" className="hover:text-cyan-400">Inicio</Link></li>
+          <li><Link to="/about" className="hover:text-cyan-400">Sobre mí</Link></li>
+          <li><Link to="/experience" className="hover:text-cyan-400">Experiencia</Link></li>
+          <li><Link to="/projects" className="hover:text-cyan-400">Proyectos</Link></li>
+          <li><Link to="/contact" className="hover:text-cyan-400">Contacto</Link></li>
+        </ul>
+        <h1 className="text-xl  font-bold">Miguel Largo</h1>
+
+      </div>
+    </nav>
+  );
+}
